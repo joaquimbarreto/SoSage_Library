ab3245b8e5be6a2334c711b7b20e908f6a85cdf6
Joaquim Barreto <joaquim@barreto.co>
2022-03-10 10:34:02 +0100

    pair programming

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,15 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Book } from 'src/app/models/book';
 
-const filteredLibraryBooks: Book[] = [
-  {title: "Harry Potter", owner: "John Smith", available: true},
-  {title: "Moby Dick", owner: "Andrea Solomon", available: false},
-  {title: "A Christmas Carol", owner: "Harry Bond", available: true},
-  {title: "The Bible", owner: "Angela Johnson", available: false},
-  {title: "Tropic of Cancer", owner: "Fabio Laurentino", available: true}
-]
-
-
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -18,12 +9,34 @@ const filteredLibraryBooks: Book[] = [
 export class HomeComponent implements OnInit {
   
   displayedColumns: string[] = ['title', 'owner', 'availability']
-  dataSource = filteredLibraryBooks;
   isAvailable = false
+  searchText = '';
+  filteredLibraryBooks: Book[] = [];
+  sampleLibraryBooks: Book[] = [
+    {title: "Harry Potter", owner: "John Smith", available: true},
+    {title: "Moby Dick", owner: "Andrea Solomon", available: false},
+    {title: "A Christmas Carol", owner: "Harry Bond", available: true},
+    {title: "The Bible", owner: "Angela Johnson", available: false},
+    {title: "Tropic of Cancer", owner: "Fabio Laurentino", available: true}
+  ]
+  dataSource: Book[] = [];
 
+  
   constructor() { }
 
   ngOnInit(): void {
+    this.filterBooks();
   }
+
+  filterBooks() {
+    if (this.searchText === '') {
+      this.filteredLibraryBooks = this.sampleLibraryBooks;
+    } else {  
+    this.filteredLibraryBooks = this.sampleLibraryBooks.filter(book => {
+      return book.title.toLowerCase().includes(this.searchText.toLowerCase())
+    })
+  }
+  this.dataSource = this.filteredLibraryBooks;
+}
 
 }
